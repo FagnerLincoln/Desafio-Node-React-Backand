cf096566710404a7e05e223fefcac973b45c3be9
fagnerlincoln <fagnerlincoln@outlook.com>
2024-08-02 01:18:05 -0300

    Atualiz Final

--- index.js
+++ index.js
@@ -7,50 +7,47 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+const orders = []; 
 
-const users = [];
-
-const checkUserId = (request, response, next) => {
+const checkOrderId = (request, response, next) => {
     const { id } = request.params;
-    const index = users.findIndex(user => user.id === id);
+    const index = orders.findIndex(order => order.id === id);
 
     if (index < 0) {
-        return response.status(404).json({ message: "User not Found" });
+        return response.status(404).json({ message: "Order not Found" });
     }
 
-    request.userIndex = index;
-    request.userId = id;
+    request.orderIndex = index;
+    request.orderId = id;
     next();
 };
 
-
-app.get('/users', (request, response) => {
-    return response.json(users);
+app.get('/orders', (request, response) => {
+    return response.json(orders);
 });
 
-app.post('/users', (request, response) => {
-    const { name, age } = request.body;
-    const user = { id: uuid.v4(), name, age };
-    users.push(user);
-    return response.status(201).json(users);
+app.post('/orders', (request, response) => {
+    const { name, pedido } = request.body;
+    const order = { id: uuid.v4(), name, pedido };
+    orders.push(order);
+    return response.status(201).json(order);
 });
 
-app.put('/users/:id', checkUserId, (request, response) => {
-    const { name, age } = request.body;
-    const index = request.userIndex;
-    const id = request.userId;
-    const updatedUser = { id, name, age };
-    users[index] = updatedUser;
-    return response.json(updatedUser);
+app.put('/orders/:id', checkOrderId, (request, response) => {
+    const { name, pedido } = request.body;
+    const index = request.orderIndex;
+    const id = request.orderId;
+    const updatedOrder = { id, name, pedido };
+    orders[index] = updatedOrder;
+    return response.json(updatedOrder);
 });
 
-app.delete('/users/:id', checkUserId, (request, response) => {
-    const index = request.userIndex;
-    users.splice(index, 1);
+app.delete('/orders/:id', checkOrderId, (request, response) => {
+    const index = request.orderIndex;
+    orders.splice(index, 1);
     return response.status(204).json();
 });
 
-
 app.listen(port, () => {
     console.log(` 💀 Server Started on port ${port} 💀 `)
-    })
+});
